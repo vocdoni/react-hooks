55201c9c10b51f344844d945236dcf5652fe72ac
lales12 <alexflores120@gmail.com>
2021-06-21 15:35:53 +0200

    [VPROD-33] Improve process hooks

---
+++ src/cache-service.tsx
@@ -0,0 +1,34 @@
+interface IOptions {
+  id: string
+  regenerate?: boolean
+}
+
+interface IUseCacheProps<T> {
+  options: IOptions
+  request: () => Promise<T>
+}
+
+const requests = new Map<string, any>([])
+
+export function cacheService<T>({
+  options,
+  request
+}: IUseCacheProps<T>): Promise<T> {
+  const promise = new Promise<T>((resolve, reject) => {
+    if (requests.has(options.id) && !options.regenerate) {
+      resolve(requests.get(options.id))
+
+      return
+    }
+
+    request()
+      .then((data: T) => {
+        requests.set(options.id, data)
+
+        resolve(data)
+      })
+      .catch(reject)
+  })
+
+  return promise
+}
